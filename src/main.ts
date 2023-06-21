e8d37370a72d75cd73d5bfbad8c3ea8c186fb515
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-06-20 22:22:33 -0500

    feat(Root): CORS
    add CORS with options as an object

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as morgan from 'morgan';
 import { AppModule } from './app.module';
+import { corsOptions } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
+
   const configService = app.get(ConfigService);
-  console.log({ port: configService.get('PORT'), env: process.env.NODE_ENV });
+
+  app.setGlobalPrefix('api');
+
+  app.enableCors(corsOptions);
+
   await app.listen(configService.get('PORT'));
+  console.log(
+    `Listening on Port ${configService.get('PORT')} in ${configService.get(
+      'NODE_ENV',
+    )} mode`,
+  );
 }
 bootstrap();
