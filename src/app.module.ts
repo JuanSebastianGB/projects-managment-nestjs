dac13c8eb438cc9cdbcc3f0d8a0042b0412d1dc8
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-06-20 22:12:02 -0500

    feat(Root): config setup
    add config setup to work with
    
     different environment files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(morgan('dev'));
+  const configService = app.get(ConfigService);
+  console.log({ port: configService.get('PORT'), env: process.env.NODE_ENV });
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
