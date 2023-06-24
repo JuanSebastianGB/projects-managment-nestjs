0cdf72277db41b7828cd106ec8efb13539670b37
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-06-23 23:23:33 -0500

    feat(Root): user-controller error-manager
    add user controller and custom error manager

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() body: UserDto) {
+    return await this.userService.createUser(body);
+  }
+
+  @Get()
+  public async findUsers() {
+    return await this.userService.findUsers();
+  }
+
+  @Get(':id')
+  async findUserById(@Param('id') id: string) {
+    return await this.userService.findUserById(id);
+  }
+
+  @Put(':id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return await this.userService.updateUser(id, body);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    return await this.userService.deleteUser(id);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ErrorManager } from 'src/utils/error.manager';
 import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserDto } from './dto/user.dto';
@@ -14,29 +15,46 @@ export class UserService {
 
   public async createUser(body: UserDto): Promise<UserEntity> {
     try {
-      const user = await this.userRepository.save(body);
+      const user: UserEntity = await this.userRepository.save(body);
+      if (!user)
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'was not possible to create the user',
+        });
       return user;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
-  public async getAllUsers(): Promise<UserEntity[]> {
+  public async findUsers(): Promise<UserEntity[]> {
     try {
+      const users: UserEntity[] = await this.userRepository.find();
+      if (users.length === 0)
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'No users found',
+        });
       return await this.userRepository.find();
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
-  public async getUserById(id: string): Promise<UserEntity | undefined> {
+  public async findUserById(id: string): Promise<UserEntity | undefined> {
     try {
-      return await this.userRepository
+      const user: UserEntity = await this.userRepository
         .createQueryBuilder('user')
         .where({ id })
         .getOne();
+      if (!user)
+        throw new ErrorManager({
+          type: 'NOT_FOUND',
+          message: 'User not found',
+        });
+      return user;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 
@@ -46,19 +64,27 @@ export class UserService {
   ): Promise<UpdateResult> {
     try {
       const user: UpdateResult = await this.userRepository.update(id, body);
-      if (user.affected === 0) return undefined;
+      if (user.affected === 0)
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'was not possible to update the user',
+        });
       return user;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
   public async deleteUser(id: string): Promise<DeleteResult | undefined> {
     try {
       const user: DeleteResult = await this.userRepository.delete(id);
-      if (user.affected === 0) return undefined;
+      if (user.affected === 0)
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'was not possible to delete the user',
+        });
       return user;
     } catch (error) {
-      throw new Error(error);
+      throw ErrorManager.createSignatureError(error.message);
     }
   }
 }
